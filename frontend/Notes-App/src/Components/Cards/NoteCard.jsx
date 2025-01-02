28e075b79e63643b5d1a8ad6a972f012563db9c5
Priyanshu Vasudev <142427709+Priyanshu2608@users.noreply.github.com>
2025-01-02 20:18:16 +0530

    SearchBar Added

---
+++ frontend/Notes-App/src/Components/Cards/NoteCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NoteCard = () => {
+  return (
+    <div>
+        kdfjkd
+      
+    </div>
+  )
+}
+
+export default NoteCard
--- frontend/Notes-App/src/Components/Navbar.jsx
+++ frontend/Notes-App/src/Components/Navbar.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, {useState} from 'react'
 import ProfileInfo from './Cards/ProfileInfo'
-
+import { useNavigate } from 'react-router-dom';
+import SearchBar from './SearchBar/SearchBar';
 const Navbar = () => {
+  const [searchQuery, setSearchQuery] = React.useState('');
+  const navigate = useNavigate;
+  const onLogout = () => {
+    navigate("/login");
+  };
+  const handleSearch = () => {
+  };
+    const onClearSearch = () => {
+      setSearchQuery("");
+    };
   return (
     <div className="bg-white flex items-center justify-between px-6 py-6 drop-shadow">
         <h2 className="text-xl font-medium text-black py-2">Notes</h2>
-
-        <ProfileInfo/>
+        <SearchBar
+        value={searchQuery}
+        onChange={({target})=>{
+        setSearchQuery(target.value);
+      }}
+      handleSearch={handleSearch}
+      onClearSearch={onClearSearch} 
+        />
+        <ProfileInfo onLogout={onLogout}/>
     </div>
   )
 }
