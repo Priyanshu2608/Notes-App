e2fd59d7ced7fe1e0fc19a90269964737481af0d
Priyanshu Vasudev <142427709+Priyanshu2608@users.noreply.github.com>
2025-01-02 12:31:15 +0530

    first Commit

---
+++ frontend/Notes-App/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Home from './pages/Home/Home.jsx'
+import Login from './pages/Login/Login.jsx'
+import Signup from './pages/Signup/Signup.jsx'
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom' 
+const routes=(
+  <Router>
+    <Routes>
+      <Route path="/dashboard" element={<Home/>} />
+      <Route path="/login" element={<Login/>} />
+      <Route path="/signup" element={<Signup/>} />
+    </Routes>
+  </Router>
+);
+const App = () => {
+  return (
+    <div>
+      {routes}
+    </div>
+  )
+}
+
+export default App
