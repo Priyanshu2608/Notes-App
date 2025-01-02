bf8c633373ef9e6f61cc351f47c99ce5ad564b84
Priyanshu Vasudev <142427709+Priyanshu2608@users.noreply.github.com>
2025-01-02 19:51:54 +0530

    navbar

--- frontend/Notes-App/src/pages/Home/Home.jsx
+++ frontend/Notes-App/src/pages/Home/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Navbar from '../../Components/Navbar'
 
 const Home = () => {
   return (
-    <div className="text-3xl font-bold underline">
-      hello
-    </div>
+    <>
+    <Navbar/>
+    </>
   )
 }
 
