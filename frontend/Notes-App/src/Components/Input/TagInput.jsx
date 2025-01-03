24267f7ab9c055be032b742018dc929c9aff7c81
Priyanshu Vasudev <142427709+Priyanshu2608@users.noreply.github.com>
2025-01-03 22:43:56 +0530

    close button

--- frontend/Notes-App/src/Components/Input/TagInput.jsx
+++ frontend/Notes-App/src/Components/Input/TagInput.jsx
@@ -24,7 +24,7 @@ const TagInput = ({tags, setTags}) => {
     <div>
           {tags?.length>0 &&<div className="flex items-center gap-2 flex-wrap mt-2">
               {tags.map((tag, index) => (
-                  <span key={index} className="">
+                  <span key={index} className="flex items-center gap-2 text-sm text-slate-900 bg-slate-100 px-3 py-1 rounded">
                       # {tag} 
                   
                   <button onClick={() => {handleRemoveTag(tag)}}>
@@ -35,7 +35,7 @@ const TagInput = ({tags, setTags}) => {
         </div>}
 
 <div className="flex items-center gap-4 mt-3">
-              <input type="text" className="text-sm bg-transparent border px-3 py-2 rounded outline-none" placeholder='ADD Tags' onChange={handleInputChange} onKeyDown={handleKeyDown} />
+              <input type="text" value={inputValue} className="text-sm bg-transparent border px-3 py-2 rounded outline-none" placeholder='ADD Tags' onChange={handleInputChange} onKeyDown={handleKeyDown} />
               <button className="w-8 h-8 flex items-center justify-center rounded border border-blue-700 hover:bg-blue-700" onClick={() => { addNewTag(); } }>
                   <MdAdd className="text-2xl text-blue-700 hover:text-white" />
               </button>
