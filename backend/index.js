bc002d69992e63680acf019d3fbde61819531b5e
Priyanshu Vasudev <142427709+Priyanshu2608@users.noreply.github.com>
2025-01-08 22:31:43 +0530

    database connected

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,13 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app= express();
+const config =require("./config.json")
+const mongoose = require("mongoose")
+mongoose.connect(config.connectionString);
+
+const jwt =require("jsonwebtoken");
+const {authenticateToken}= require("./utilities")
 
 app.use(express.json());
 app.use(
@@ -12,6 +19,26 @@ app.use(
 app.get("/",(req,res)=>{
     res.json({data:"Hello World"}); 
 });
+
+app.post("/create-account", async(req,res)=>{
+    const {fullName, email, password} =req.body;
+    if(!fullName) {
+        return res
+        .status(400)
+        .json({error: true, message: "Full name is required"});
+
+    }
+    if(!email) {
+        return res
+        .status(400)
+        .json({error: true, message : "Email is required"});
+    }
+    if(!password){
+        return res 
+        .status(400)
+        .json({error: true, message : "Passowrd Reqiuired"})
+    }
+})
 
 app.listen(8000);
 module.exports = app;
