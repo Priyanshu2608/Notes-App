c367e45b8a9ae3000a2737f438057d1cdc493cf3
Priyanshu Vasudev <142427709+Priyanshu2608@users.noreply.github.com>
2025-06-02 12:25:55 +0530

    Changes

--- frontend/Notes-App/src/App.jsx
+++ frontend/Notes-App/src/App.jsx
@@ -3,10 +3,11 @@ import Home from './pages/Home/Home.jsx'
 import Login from './pages/Login/Login.jsx'
 import Signup from './pages/Signup/Signup.jsx'
 
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom' 
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom' 
 const routes=(
   <Router>
     <Routes>
+      <Route path="/" element={<Navigate to="/login" />} />
       <Route path="/dashboard" element={<Home/>} />
       <Route path="/login" element={<Login/>} />
       <Route path="/signup" element={<Signup/>} />
